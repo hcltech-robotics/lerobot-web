01b14ceb87ba01999004f22327350764d12b9252
Vincze Péter <vinczep@starschema.onmicrosoft.com>
2025-06-27 16:31:00 +0200

    remove unnecessary comment

--- lerobot-web-fe/src/hooks/useRobotStatus.ts
+++ lerobot-web-fe/src/hooks/useRobotStatus.ts
@@ -9,7 +9,6 @@ export function useRobotStatus() {
     const fetchData = async () => {
       try {
         const { robot_status } = await getStatus();
-        //filter out the so-100 model because status response can contain other kind of models.
         const filteredRobotArms = robot_status.filter((robot) => robot.name === ROBOT_MODEL_SO_100);
         setRobotStatus(filteredRobotArms);
       } catch (error) {
