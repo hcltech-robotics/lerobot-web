f112a16ff768fdc6aae4694de1be4082da631b1f
Vincze Péter <vinczep@starschema.onmicrosoft.com>
2025-07-02 16:10:51 +0200

    Reorganize the folder structure and refactor the layout

--- lerobot-web-fe/src/App.tsx
+++ lerobot-web-fe/src/App.tsx
@@ -1,28 +1,5 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Layout from './components/Layout';
-import { lazy, Suspense } from 'react';
-import Loader from './components/Loader';
+import AppRouter from './routers/AppRouter';
 
-const Teleoperate = lazy(() => import('./components/Teleoperate'));
-const Calibration = lazy(() => import('./components/Calibration'));
-const Policies = lazy(() => import('./components/Policies'));
-const AITraining = lazy(() => import('./components/AITraining'));
-
-export default function AppRouter() {
-  return (
-    <Router>
-      <Suspense fallback={<Loader />}>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Navigate to="/teleoperate" />} />
-            <Route path="teleoperate" element={<Teleoperate />} />
-            <Route path="calibration" element={<Calibration />} />
-            <Route path="policies" element={<Policies />} />
-            <Route path="ai-training" element={<AITraining />} />
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Route>
-        </Routes>
-      </Suspense>
-    </Router>
-  );
+export default function App() {
+  return <AppRouter />;
 }
