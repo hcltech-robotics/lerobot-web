89a79c143f5eda8b36da1566414b8b784d94dccc
Vincze Péter <vinczep@starschema.onmicrosoft.com>
2025-07-16 12:43:26 +0200

    change destruct, to separated selector-based solution

--- lerobot-web-fe/src/App.tsx
+++ lerobot-web-fe/src/App.tsx
@@ -4,8 +4,10 @@ import { useStatusStore } from './stores/status.store';
 import { useStatusPolling } from './hooks/useStatusPolling';
 
 export default function App() {
+  const apiUrl = useStatusStore((s) => s.apiUrl);
+  const setApiUrl = useStatusStore((s) => s.setApiUrl);
+
   useEffect(() => {
-    const { apiUrl, setApiUrl } = useStatusStore.getState();
     if (!apiUrl) {
       setApiUrl('http://127.0.0.1:8000');
     }
