d17beafb773b98ac370060eff8575fcbe06aa3d2
Mark Maroti <mark.maroti@gmail.com>
2025-07-24 09:45:53 +0200

    feat: add configuratin form integration

--- lerobot-web-fe/src/App.tsx
+++ lerobot-web-fe/src/App.tsx
@@ -1,18 +1,7 @@
-import { useEffect } from 'react';
 import AppRouter from './routers/AppRouter';
-import { useStatusStore } from './stores/status.store';
 import { useStatusPolling } from './hooks/useStatusPolling';
 
 export default function App() {
-  const apiUrl = useStatusStore((s) => s.apiUrl);
-  const setApiUrl = useStatusStore((s) => s.setApiUrl);
-
-  useEffect(() => {
-    if (!apiUrl) {
-      setApiUrl('http://127.0.0.1:8000');
-    }
-  }, []);
-
   useStatusPolling();
 
   return (
--- lerobot-web-fe/src/pages/Configuration.tsx
+++ lerobot-web-fe/src/pages/Configuration.tsx
@@ -1,51 +1,75 @@
 import { useState } from 'react';
 import * as Form from '@radix-ui/react-form';
 import { EyeClosedIcon, EyeOpenIcon } from '@radix-ui/react-icons';
+import { useStatusStore } from '../stores/status.store';
+import { isValidUrl } from '../services/configuration.service';
 
 import styles from './Configuration.module.css';
 
 export default function Configuration() {
+  const setApiUrl = useStatusStore((state) => state.setApiUrl);
+  const apiUrl = useStatusStore((state) => state.apiUrl);
+  const setToken = useStatusStore((state) => state.setToken);
+  const token = useStatusStore((state) => state.token);
+
+  const [url, setUrl] = useState(apiUrl || '');
+  const [localToken, setLocalToken] = useState(token || '');
   const [showPassword, setShowPassword] = useState(false);
+  const [urlTouched, setUrlTouched] = useState(false);
+
+  const isUrlValid = isValidUrl(url);
+  const isFormValid = isUrlValid;
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!isFormValid) return;
+
+    setApiUrl(url.trim());
+    setToken(localToken);
+  };
 
   return (
     <section className={styles.ConfigurationWrapper}>
       <h2 className={styles.Title}>Configuration</h2>
-      <Form.Root className={styles.Form}>
+      <Form.Root onSubmit={handleSubmit}>
         <Form.Field className={styles.Field} name="url">
           <div className={styles.MessageContainer}>
             <Form.Label className={styles.Label}>Backend Url</Form.Label>
-            <Form.Message className={styles.ErrorMessage} match="valueMissing">
-              Please enter a URL
-            </Form.Message>
-            <Form.Message className={styles.ErrorMessage} match="typeMismatch">
-              Please provide a valid URL
-            </Form.Message>
+
+            {urlTouched && !isUrlValid && <div className={styles.ErrorMessage}>Please provide a valid URL.</div>}
           </div>
-          <Form.Control asChild>
-            <input className={styles.Input} type="url" name="url" required />
-          </Form.Control>
+
+          <Form.Control
+            className={styles.Input}
+            type="url"
+            required
+            name="url"
+            value={url || ''}
+            onChange={(e) => setUrl(e.target.value)}
+            onBlur={() => setUrlTouched(true)}
+          />
         </Form.Field>
 
         <Form.Field className={styles.Field} name="token">
           <div className={styles.MessageContainer}>
             <Form.Label className={styles.Label}>Hugging Face Token</Form.Label>
-            <Form.Message className={styles.ErrorMessage} match="valueMissing">
-              Please enter a token
-            </Form.Message>
           </div>
           <div className={styles.TokenWrapper}>
-            <Form.Control asChild>
-              <input className={`${styles.Input} ${styles.TokenInput}`} type={showPassword ? 'text' : 'password'} name="token" required />
-            </Form.Control>
-            <button type='button' className={styles.TokenIconButton} onClick={() => setShowPassword(!showPassword)}>
+            <Form.Control
+              className={`${styles.Input} ${styles.TokenInput}`}
+              type={showPassword ? 'text' : 'password'}
+              name="token"
+              value={localToken}
+              onChange={(e) => setLocalToken(e.target.value)}
+            />
+            <button type="button" className={styles.TokenIconButton} onClick={() => setShowPassword(!showPassword)}>
               {showPassword ? <EyeOpenIcon /> : <EyeClosedIcon />}
             </button>
           </div>
         </Form.Field>
-        <Form.Submit asChild>
-          <button type='submit' className={styles.SubmitButton}>
-            Save
-          </button>
+
+        <Form.Submit className={styles.SubmitButton} disabled={!isFormValid}>
+          Save
         </Form.Submit>
       </Form.Root>
     </section>
