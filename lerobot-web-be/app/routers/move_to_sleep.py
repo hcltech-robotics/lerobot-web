f16d0ec85eaac39151b9533697c9c722641b9df8
Vincze Péter <vinczep@starschema.onmicrosoft.com>
2025-08-05 12:42:45 +0200

    refactor the move_to_sleep endpoint, to apply robot_id and have a delay before disconnect

--- lerobot-web-be/app/routers/move_to_sleep.py
+++ lerobot-web-be/app/routers/move_to_sleep.py
@@ -1,27 +1,44 @@
-from fastapi import APIRouter
+import logging
+from time import sleep
 
+from fastapi import APIRouter
 from lerobot.robots.so100_follower import SO100Follower, SO100FollowerConfig
+from pydantic import BaseModel
 
 router = APIRouter()
+logger = logging.getLogger(__name__)
 
 
-@router.get("/move_to_sleep")
-def move_to_sleep():
-    config = SO100FollowerConfig(port="/dev/tty.usbmodem58FA1019351", use_degrees=True)
+class MoveToSleepRequest(BaseModel):
+    follower_id: str
 
-    robot = SO100Follower(config)
 
-    robot.connect(calibrate=False)
+@router.post("/move_to_sleep")
+def move_to_sleep(params: MoveToSleepRequest):
+    try:
 
-    middle_state = {
-        "shoulder_pan.pos": -0.50,
-        "shoulder_lift.pos": -119.8,
-        "elbow_flex.pos": 70.0,
-        "wrist_flex.pos": 50.0,
-        "wrist_roll.pos": 0.2,
-        "gripper.pos": 0.72,
-    }
+        config = SO100FollowerConfig(
+            port=f"/dev/tty.usbmodem{params.follower_id}", use_degrees=True
+        )
+        robot = SO100Follower(config)
 
-    robot.send_action(middle_state)
+        robot.connect(calibrate=False)
 
-    robot.disconnect()
+        middle_state = {
+            "shoulder_pan.pos": -0.50,
+            "shoulder_lift.pos": -119.8,
+            "elbow_flex.pos": 70.0,
+            "wrist_flex.pos": 50.0,
+            "wrist_roll.pos": 0.2,
+            "gripper.pos": 0.72,
+        }
+
+        robot.send_action(middle_state)
+    except Exception as e:
+        logger.error(f"Failed to move to sleep: {e}")
+        return {"status": "error", "message": f"Failed to move to sleep: {e}"}
+    finally:
+        sleep(1)
+        if robot and robot.is_connected:
+            robot.disconnect()
+    return {"status": "ok", "message": "Robot arm moved to sleep"}
