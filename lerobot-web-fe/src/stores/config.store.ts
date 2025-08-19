b6b3fea7770d6a73ba2f9dc58060aedd6e86bd81
Mark Maroti <mark.maroti@gmail.com>
2025-08-19 19:08:12 +0200

    chore: restore back the configuration page

--- lerobot-web-fe/src/stores/config.store.ts
+++ lerobot-web-fe/src/stores/config.store.ts
@@ -4,19 +4,27 @@ import { immer } from 'zustand/middleware/immer';
 
 interface ConfigState {
   apiUrl: string | null;
+  token: string | null;
 }
 
 interface ConfigActions {
   setApiUrl: (url: string) => void;
+  setToken: (token: string) => void;
 }
 
 export const useConfigStore = create<ConfigState & ConfigActions>()(
   persist(
     immer((set) => ({
       apiUrl: null,
+      token: null,
       setApiUrl: (url) => {
         set((state) => {
           state.apiUrl = url;
+        });
+      },
+      setToken: (token) => {
+        set((state) => {
+          state.token = token;
         });
       },
     })),
@@ -24,6 +32,7 @@ export const useConfigStore = create<ConfigState & ConfigActions>()(
       name: 'config',
       partialize: (state) => ({
         apiUrl: state.apiUrl,
+        token: state.token,
       }),
     },
   ),
