b16484912853f93beaa144535f845e0d18f061f8
Mark Maroti <mark.maroti@gmail.com>
2025-10-02 16:45:03 +0200

    feat: add basic live chart component for joint states

--- lerobot-web-fe/src/components/Header.tsx
+++ lerobot-web-fe/src/components/Header.tsx
@@ -1,4 +1,5 @@
 import RobotIconContainer from './RobotIconContainer';
+import { LiveChart } from './LiveChart';
 
 import styles from './Header.module.css';
 
@@ -6,6 +7,9 @@ export default function Header() {
   return (
     <header className={styles.header}>
       <h1 className={styles.title}>Lerobot Robot Arm</h1>
+      <div className={styles.chartWrapper}>
+        <LiveChart />
+      </div>
       <RobotIconContainer />
     </header>
   );
