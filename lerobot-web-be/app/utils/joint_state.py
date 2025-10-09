0b4b98d39173fa18f08f1d863c9bf7ad1a9a3eb4
Vincze Péter <vinczep@starschema.onmicrosoft.com>
2025-10-09 10:52:55 +0200

    fix joint_state logic to remap the joint states

--- lerobot-web-be/app/utils/joint_state.py
+++ lerobot-web-be/app/utils/joint_state.py
@@ -6,9 +6,24 @@ from ..models.joint_state import JOINT_NAME_MAP
 def remap_keys_for_client_and_convert_to_deg(
     raw_joint_states: dict[str, float],
 ) -> dict[str, float]:
+    mapped = {}
+    for backend_key, frontend_key in JOINT_NAME_MAP.items():
+        if backend_key in raw_joint_states:
+            val = raw_joint_states[backend_key]
+            if backend_key == "wrist_roll.pos" or backend_key == "shoulder_pan.pos":
+                val = -val
+            mapped[frontend_key] = math.radians(val)
+    return mapped
 
-    return {
-        frontend_key: math.radians(raw_joint_states[backend_key])
-        for backend_key, frontend_key in JOINT_NAME_MAP.items()
-        if backend_key in raw_joint_states
-    }
+
+def remap_keys_for_backend_and_convert_to_rad(
+    frontend_joint_states: dict[str, float],
+) -> dict[str, float]:
+    mapped = {}
+    for backend_key, frontend_key in JOINT_NAME_MAP.items():
+        if frontend_key in frontend_joint_states:
+            val = frontend_joint_states[frontend_key]
+            if frontend_key == "wristRoll" or frontend_key == "rotation":
+                val = -val
+            mapped[backend_key] = math.degrees(val)
+    return mapped
