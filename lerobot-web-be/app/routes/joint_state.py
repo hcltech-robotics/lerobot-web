2047591905a053f5f80553bd6912e5022602db34
Vincze Péter <vinczep@starschema.onmicrosoft.com>
2025-11-04 15:45:06 +0100

    fix bimanual teleoperation and joint state manager

--- lerobot-web-be/app/routes/joint_state.py
+++ lerobot-web-be/app/routes/joint_state.py
@@ -48,4 +48,4 @@ def get_state(req: JointStateRequest):
 @router.websocket("/ws/joint_state")
 async def websocket_joint_state(websocket: WebSocket, follower_id: str = Query(...)):
     await websocket.accept()
-    await teleop_manager.stream_joint_states(websocket)
+    await teleop_manager.stream_joint_states(websocket, follower_id)
--- lerobot-web-be/app/services/teleoperate.py
+++ lerobot-web-be/app/services/teleoperate.py
@@ -45,9 +45,7 @@ class TeleoperationManager:
             leader_id = list(leader_map.values())[0]
             follower_id = list(follower_map.values())[0]
             leader_config = SO100LeaderConfig(port=f"{prefixes[0]}{leader_id}")
-            follower_config = SO100FollowerConfig(
-                port=f"{prefixes[0]}{follower_id}"
-            )
+            follower_config = SO100FollowerConfig(port=f"{prefixes[0]}{follower_id}")
             self.leader_arm = SO100Leader(leader_config)
             self.follower_arm = SO100Follower(follower_config)
 
@@ -58,9 +56,16 @@ class TeleoperationManager:
         if not self.follower_arm or not self.leader_arm:
             logger.error("Arms not initialized before teleopeation loop")
             return
-
-        self.leader_arm.connect(calibrate=False)
-        self.follower_arm.connect(calibrate=False)
+        try:
+            self.leader_arm.connect(calibrate=False)
+        except Exception as e:
+            logger.error(f"Error during connection to leader arm: {e}")
+            return
+        try:
+            self.follower_arm.connect(calibrate=False)
+        except Exception as e:
+            logger.error(f"Error during connection to leader arm: {e}")
+            return
         if not self.leader_arm.is_connected or not self.follower_arm.is_connected:
             raise RuntimeError("Leader or follower arm failed to connect.")
 
@@ -68,10 +73,24 @@ class TeleoperationManager:
 
     def disconnect_arms(self, move_to_sleep: bool = False):
         if self.leader_arm and self.leader_arm.is_connected:
-            self.leader_arm.disconnect()
+            try:
+                self.leader_arm.disconnect()
+            except Exception as e:
+                logger.error(f"Error during connection to leader arm: {e}")
+
         if self.follower_arm and self.follower_arm.is_connected:
             if move_to_sleep:
-                self.follower_arm.send_action(sleep_position)
+                if self.is_bi_setup:
+                    action_dict = {}
+                    action_dict.update(
+                        {f"left_{key}": value for key, value in sleep_position.items()}
+                    )
+                    action_dict.update(
+                        {f"right_{key}": value for key, value in sleep_position.items()}
+                    )
+                    self.follower_arm.send_action(action_dict)
+                else:
+                    self.follower_arm.send_action(sleep_position)
             time.sleep(2)
             self.follower_arm.disconnect()
 
@@ -80,7 +99,7 @@ class TeleoperationManager:
         logger.info("Arms disconnected")
 
     # ---------- TELEOPERATION ----------
-    async def _teleoperate_loop(self, fps: int):
+    async def _teleoperate_loop(self, fps: int, follower_map: dict):
         logger.info("Teleoperation loop started")
         if not self.follower_arm or not self.leader_arm:
             logger.error("Arms not initialized before teleopeation loop")
@@ -93,8 +112,32 @@ class TeleoperationManager:
                 await asyncio.to_thread(self.follower_arm.send_action, action)
 
                 try:
-                    obs = remap_keys_for_client_and_convert_to_deg(action)
-                    self.joint_state_queue.put_nowait(obs)
+                    if self.is_bi_setup:
+                        left_action_map = {}
+                        right_action_map = {}
+                        prefixes = get_serial_prefixes()
+
+                        left_arm_port = f"{prefixes[0]}{follower_map['left']}"
+                        right_arm_port = f"{prefixes[0]}{follower_map['right']}"
+                        for k, v in action.items():
+                            if k.startswith("left_"):
+                                left_action_map[k[len("left_") :]] = v
+                            if k.startswith("right_"):
+                                right_action_map[k[len("right_") :]] = v
+                        left_obs = remap_keys_for_client_and_convert_to_deg(
+                            left_action_map
+                        )
+                        right_obs = remap_keys_for_client_and_convert_to_deg(
+                            right_action_map
+                        )
+                        item = {left_arm_port: left_obs, right_arm_port: right_obs}
+                        self.joint_state_queue.put_nowait(item)
+
+                    else:
+                        robot_arm_port = list(follower_map.values())[0]
+                        obs = remap_keys_for_client_and_convert_to_deg(action)
+                        item = {robot_arm_port: obs}
+                        self.joint_state_queue.put_nowait(item)
                 except asyncio.QueueFull:
                     pass
 
@@ -113,7 +156,9 @@ class TeleoperationManager:
 
         self.connect_arms(leader_map, follower_map, is_bi_setup)
         self._stop_event.clear()
-        self._teleop_task = asyncio.create_task(self._teleoperate_loop(fps))
+        self._teleop_task = asyncio.create_task(
+            self._teleoperate_loop(fps, follower_map)
+        )
 
     async def stop_teleoperation(self):
         if not self._teleop_task or not self.follower_arm:
@@ -128,7 +173,7 @@ class TeleoperationManager:
         self._teleop_task = None
 
     # ---------- JOINT STATE STREAM ----------
-    async def stream_joint_states(self, websocket, fps: int = 60):
+    async def stream_joint_states(self, websocket, follower_id: str, fps: int = 60):
         if not self.follower_arm or not self.follower_arm.is_connected:
             await websocket.send_json({"error": "Follower not connected"})
             await websocket.close()
@@ -136,12 +181,21 @@ class TeleoperationManager:
         try:
             while not self._stop_event.is_set():
                 joint_states_map = await self.joint_state_queue.get()
-                await websocket.send_json(
-                    {
-                        "timestamp": time.time(),
-                        "jointState": joint_states_map,
-                    }
-                )
+                matched_state = None
+                for port, obs in joint_states_map.items():
+                    if follower_id in port:
+                        matched_state = obs
+                if matched_state is not None:
+                    await websocket.send_json(
+                        {
+                            "timestamp": time.time(),
+                            "jointState": matched_state,
+                        }
+                    )
+                else:
+                    print(
+                        f"The robot_id: {follower_id} is not accessible in the joint state map"
+                    )
                 await asyncio.sleep(1 / fps)
         except WebSocketDisconnect:
             logger.info("WebSocket client disconnected")
