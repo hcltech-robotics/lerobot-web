7203b4d8dfa144f4568198d201a0a3c19ed00296
Mark Maroti <mark.maroti@gmail.com>
2025-12-18 12:08:51 +0100

    feat: add joint state handler to groot

--- lerobot-web-be/app/services/groot.py
+++ lerobot-web-be/app/services/groot.py
@@ -1,10 +1,14 @@
 import logging
 import os
+import re
+import ast
 from typing import AsyncIterator
 
 from app.config import Settings
 from app.models.groot import GrootRequest, GrootStatusResponse
 from app.utils.serial_prefixes import get_serial_prefixes
+from app.services.joint_state_manager import joint_state_manager
+from app.utils.joint_state import remap_keys_for_client_and_convert_to_deg
 
 logger = logging.getLogger(__name__)
 
@@ -21,6 +25,7 @@ class GrootService:
     def __init__(self, process_manager, settings: Settings):
         self._manager = process_manager
         self._settings = settings
+        self._robot_port = ""
 
     def status(self) -> GrootStatusResponse:
         is_running = self._manager.is_running()
@@ -31,6 +36,7 @@ class GrootService:
         if self._manager.is_running():
             raise GrootAlreadyRunningError("Groot process already running")
 
+        self._robot_port = req.robot_port
         prefixes = get_serial_prefixes(req.robot_port)
         prefixed_robot_port = f"{prefixes[0]}{req.robot_port}"
         args = [
@@ -67,6 +73,23 @@ class GrootService:
             logger.info("Stopping Groot process (pid=%s)", self._manager.proc.pid)
         await self._manager.stop()
 
-    async def stream_lines(self) -> AsyncIterator[str]:
+    """ async def stream_lines(self) -> AsyncIterator[str]:
         async for line in self._manager.stream_lines():
+            yield line """
+
+    async def stream_lines(self) -> AsyncIterator[str]:
+        yield "Running..."
+        async for line in self._manager.stream_lines():
+            try:
+                if "Sending action:" in line:
+                    match = re.search(r"\{.*\}", line)
+                    if match:
+                        obj_str = match.group(0)
+                        action_dict = ast.literal_eval(obj_str)
+                        obs = remap_keys_for_client_and_convert_to_deg(action_dict)
+                        joint_state_manager.publish(self._robot_port, obs)
+
+            except Exception:
+                logger.exception("Failed to parse action line: %s", line)
+
             yield line
