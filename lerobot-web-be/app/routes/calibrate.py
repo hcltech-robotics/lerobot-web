9b37f220c3a23de1d327cf2050795367b21c8fbc
Vincze Péter <vinczep@starschema.onmicrosoft.com>
2025-12-18 10:41:02 +0100

    Update calibration process

--- lerobot-web-be/app/routes/calibrate.py
+++ lerobot-web-be/app/routes/calibrate.py
@@ -1,21 +1,21 @@
 from fastapi import APIRouter
 
 from ..models.calibrate import CalibrationParams
-from ..services.calibrate import confirm_enter, confirm_start, start_calibration
+from ..services.calibrate import confirm_start, confirm_step, start_calibration
 
 router = APIRouter()
 
 
 @router.post("/calibrate/start", tags=["calibrate"])
-def start(params: CalibrationParams):
-    return start_calibration(params)
+async def start(params: CalibrationParams):
+    return await start_calibration(params)
 
 
 @router.post("/calibrate/confirm_calibration_start", tags=["calibrate"])
 async def confirm_calibration_start():
-    return confirm_start()
+    return await confirm_start()
 
 
 @router.post("/calibrate/confirm_calibration_step", tags=["calibrate"])
 async def confirm_calibration_step():
-    return confirm_enter()
+    return await confirm_step()
